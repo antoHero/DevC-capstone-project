c7baf6d4a40f343b136a5208b42575850eef18db
antoHero <veeqanto@gmail.com>
2019-11-29 21:00:43 +0100

    delete gif

--- backend/controllers/Gif.js
+++ backend/controllers/Gif.js
@@ -3,7 +3,7 @@ import db from '../database/db';
 const Gif = {
 
     //create or add a gif
-    async addArticle(req, res) {
+    async addGif(req, res) {
         const createQuery = `INSERT INTO 
         gif(id, title, imageUrl, createdOn, user_id)
         VALUES($1,$2,$3,$4,$5)
@@ -22,6 +22,21 @@ const Gif = {
             return res.status(400).send(error);
         }
     },
+
+    //employees can delete their gif images
+    async deleteGif(req, res) {
+        const deleteQuery = 'DELETE * FROM gif WHERE id=$1 AND user_id=$2 returning *';
+        try {
+            const { rows } = await db.query(deleteQuery, [req.params.id, req.user.id]);
+            if(!rows[0]) {
+                return res.status(404).json({message: 'Oops Gif not found!'});
+            }
+            return res.status(200).json({message: 'Gif deleted successfully'});
+        } catch(err) {
+            return res.status(400).json({err});
+        }
+    },
+    
 }
 
 module.exports = {Gif}
